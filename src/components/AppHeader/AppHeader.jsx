4118efbb020013449d08d75fa960ba3ac1694c44
Sergey <tigra2004@gmial.com>
2021-07-21 21:47:30 +1100

    feat: создать модуль css

---
+++ src/components/AppHeader/AppHeader.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import stylesAppHeader from './AppHeader.module.css';
+
+function AppHeader(props) {
+  return <div className={stylesAppHeader.header}></div>;
+}
+
+export default AppHeader;
