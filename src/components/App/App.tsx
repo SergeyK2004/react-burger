569ae45e94e215c069b69edeb704b98af7c90bb6
Sergey <tigra2004@gmial.com>
2021-07-22 12:09:29 +1100

    feat: создать шапку

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import AppHeader from '../AppHeader/AppHeader';
 
 function App() {
   return (
     <div className="App">
+      <AppHeader />
     </div>
   );
 }
